3b1948358bf404503b3fdc35410ce4a17053f688
Halil İbrahim Badem <badem@Halil-MacBook-Air.local>
2022-10-05 22:52:20 +0300

    Add tours title

--- src/Tours.js
+++ src/Tours.js
@@ -3,6 +3,7 @@ import Tour from './Tour';
 const Tours = ({tours}) => {
     return (
         <section className='tours'>
+          <h1 className='tours__title'>Our Tours</h1>
             {tours.map(tour => {
               return (
                 <Tour key={tour.id} {...tour} />
