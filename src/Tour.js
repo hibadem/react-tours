65994e2e6a116529838412efb21b4385d7853a51
Halil İbrahim Badem <badem@Halil-MacBook-Air.local>
2022-10-05 22:31:59 +0300

    Add card header

--- src/Tour.js
+++ src/Tour.js
@@ -4,8 +4,10 @@ const Tour = ({id, image, info, price, name }) => {
     return (
         <article className="tour">
             <img class="tour__image" src={image} alt={name} />
-            <h2 className="tour__title">{name}</h2>
-            <span className="tour__price">{price}</span>
+            <div className="tour__header">
+                <h2 className="tour__title">{name}</h2>
+                <span className="tour__price">$ {price}</span>
+            </div>
             <div className="tour__info">{info}</div>
         </article>
     )
